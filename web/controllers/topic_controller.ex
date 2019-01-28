c8f5257f4b79b08e67327631909f2f8e2585b34f
joshuaalawode <joshalawode@gmail.com>
2019-01-27 22:05:14 -0500

    Get all the topics in the database

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,12 +3,25 @@ defmodule Discuss.TopicController do
   
   alias Discuss.Topic
 
+  @doc """
+    Display a list of topics
+  """
+  def index(conn, _params) do
+    topics = Repo.all(Topic) # get all the topics in the database - remember we are using alias here so no need to write Discuss.Repo.all(Discuss.Topic)
+  end
+
+  @doc """
+    Display the form to create a new topic
+  """
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
+  @doc """
+    Submit the form to create a new topic
+  """
   def create(conn, %{"topic" => topic}) do
     # Create the changeset - it contains the data we want to insert
     changeset = Topic.changeset(%Topic{}, topic)
@@ -20,4 +33,5 @@ defmodule Discuss.TopicController do
         render conn, "new.html", changeset: changeset # If the insertion fails, show the user the form again
     end
   end
+
 end
