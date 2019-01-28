02e8a8df7858e1d6dfb65c999d5865145980cb88
joshuaalawode <joshalawode@gmail.com>
2019-01-27 21:32:56 -0500

    If error occurs during database insertion, render the form again and show the error message

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -16,7 +16,8 @@ defmodule Discuss.TopicController do
     # Insert the data in the changeset into the database
     case Repo.insert(changeset) do
       {:ok, post} -> IO.inspect(post) # data was successfully inserted
-      {:error, changeset} -> IO.inspect(changeset) # data was not inserted
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset # If the insertion fails, show the user the form again
     end
   end
 end
