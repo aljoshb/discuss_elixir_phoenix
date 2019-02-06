66d5013f158a3da4ea6934675af0128ff9911d89
joshuaalawode <joshalawode@gmail.com>
2019-02-05 22:27:55 -0500

    OAuth signin flow working

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -3,9 +3,37 @@ defmodule Discuss.AuthController do
   plug Ueberauth
 
   alias Discuss.User
-  
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} -> # Succesful signin
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  @doc """
+    First check if the user already exists in the database, if it doesn't this is the first sign in of the user
+  """
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
