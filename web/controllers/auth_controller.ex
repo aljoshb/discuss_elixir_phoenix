a29329545909e39e91ad34fb1fd929f75fa4e027
joshuaalawode <joshalawode@gmail.com>
2019-02-06 21:42:36 -0500

    Signout button works correctly

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,6 +11,12 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true) # This is saying, drop/delete every data we have on the current user
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} -> # Succesful signin
