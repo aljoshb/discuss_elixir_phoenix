52a54fa1c0e4e239b157ec4b93ae1d7dbaa3a050
joshuaalawode <joshalawode@gmail.com>
2019-02-09 20:36:13 -0500

    Comment can be added to database

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,18 +1,29 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  alias Discuss.Topic
+  alias Discuss.{Topic, Comment}
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
     topic = Repo.get(Topic, topic_id)
     
-    {:ok, %{}, socket}
+    # Add the topic to the socket connection
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
   def handle_in(name, %{"content" => content}, socket) do
-    IO.inspect(message)
-    # This response is for phoenix, not the client
-    {:reply, :ok, socket}
+    # Get the topic that's associated with this comments socket connection
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
